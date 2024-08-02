71b8817189189f2975483a6fffd43f44c1b5bd17
GOPU KARTHIK <2000030281cse@gmail.com>
2024-08-02 11:29:52 +0530

    third

--- DAY-4/routeangular/src/app/components/home/home.component.ts
+++ DAY-4/routeangular/src/app/components/home/home.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../user.service';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CardModule,ButtonModule,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  users:any[] = [];
+  constructor(private userService:UserService){}
+  ngOnInit():void{
+    this.userService.getUser().subscribe((data)=>{
+      this.users =  data;
+    });
+  }
+
 
 }
